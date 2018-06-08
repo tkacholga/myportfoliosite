39983ddf7cb4c3b2b03d83926ee931aa3ab7e972
Olga Tkach <tkacholya@gmail.com>
2018-06-08 14:33:45 -0400

    Designing the Landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,69 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <h1>Landing Page</h1>
+      <div style={{ width: '100%', margin: 'auto' }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://forum.gethopscotch.com/uploads/default/original/3X/2/9/29a23cb9529a75f132c88f8a3c8a1b42b64d1f78.PNG"
+              alt="avatar"
+              className="avatar-img"
+            />
+            <div className="banner-text">
+              <h1>Front-End Developer</h1>
+
+              <hr />
+
+              <p>HTML/CSS | Bootsrap | JavaScript | React | NodeJS | MongoDB</p>
+
+              <div className="social-links">
+                {/* LinkedIn  */}
+                <a
+                  href="http://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+                {/* Github  */}
+                <a
+                  href="http://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+                {/* Freecodecamp  */}
+                <a
+                  href="http://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-free-code-camp" aria-hidden="true" />
+                </a>
+                {/* Twitter  */}
+                <a
+                  href="http://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-twitter-square" aria-hidden="true" />
+                </a>
+                {/* Facebook  */}
+                <a
+                  href="http://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-facebook-square" aria-hidden="true" />
+                </a>
+              </div>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
